ef7173b497b5863d80f18d55ccb5c5155ea1dfe3
Leontin <leo_l3o@yahoo.com>
2024-01-29 21:02:33 +0200

    Primele proiecte

---
+++ modul2/app4.py
@@ -0,0 +1,12 @@
+num1 = int(input('num1: '))
+num2 = int(input('num2: '))
+
+print((num1 + num2) / 2)
+
+# Integer division
+int_division = num1 // num2
+print("Integer division =", int_division)
+
+# num1 ** num2
+power = num1 ** num2
+print("num1 ** num2 =", power)
