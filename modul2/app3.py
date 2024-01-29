f922ca340bb815146fc4e93d34154e08bc90d289
Leontin <leo_l3o@yahoo.com>
2024-01-29 20:45:22 +0200

    Primele proiecte

---
+++ modul2/app3.py
@@ -0,0 +1,9 @@
+#salvare parola
+password = 7715
+user_pass = input("Enter Password:")
+print(user_pass)
+
+print(str(password) == user_pass)
+
+
+
---
+++ modul2/part1.3.py
@@ -0,0 +1,10 @@
+#compararea
+
+print(10>11)
+print(type(10>11))
+
+
+
+#bool
+print(False)
+print(True)
