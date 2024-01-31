d7327370954c698057caae5de83da385c9de62c8
Leontin <leo_l3o@yahoo.com>
2024-01-31 21:11:05 +0200

    Primele proiecte

---
+++ modul2/app9.py
@@ -0,0 +1,3 @@
+cuvant = input("Introduceti un cuvant:")
+print(f"Palindrom {cuvant == cuvant[::1]}")
+
--- modul2/part2.2..py
+++ modul2/part2.2..py
@@ -2,14 +2,14 @@
 
 #method format
 
-my_string = "text {}"
-print(my_string.format('''exemple'''))
+'''my_string = "text {}"
+print(my_string.format(''exemple''))
 my_string = "text {}"
 print(my_string.format(2))
 my_str = "text {1} {0}"
 print(my_str.format("example1", "exemple2"))
 my_str = "text {ex1} {ex2}"
-print(my_str.format(ex1="masina",ex2="tigaie"))
+print(my_str.format(ex1="masina",ex2="tigaie"))'''
 
 #formated string
 ex1 = 'Python'
@@ -26,5 +26,13 @@ print(f"text2 {ex1} {ex2} {ex3}")
 ex1 = "exemple1"
 print(len(ex1))
 
+#index of string
 
-
+hello = "hello world"
+print(hello[3])
+print(hello[3:7]) # last index not included
+print(hello[0:10:2])
+print(hello[0:12:2]) #not all version of python 3
+print(hello[::2])  #start from the first to the last index
+print(hello[::-1])  # star from the back (in ordine inversa)
+print(hello[-5:-1:])
