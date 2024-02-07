007312d931489514cfd8c8c940639f70f86807f7
Leontin <leo_l3o@yahoo.com>
2024-02-07 20:07:21 +0200

    Primele proiecte

--- modul3/app4.py
+++ modul3/app4.py
@@ -1,4 +1,4 @@
-duplicated = [1,3,5,7,3,8,9,1,4,2]    # remove duplicated numbers from the list
+duplicated = [1,3,5,7,7,7,7,7,9,3]    # remove duplicated numbers from the list
 new_list= []
 for number in duplicated:
     if number not in new_list:
