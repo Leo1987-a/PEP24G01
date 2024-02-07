25a9f19274f1e2e4a863a5bec457c208213a8b61
Leontin <leo_l3o@yahoo.com>
2024-02-07 21:31:04 +0200

    Primele proiecte

--- modul3/app3.py
+++ modul3/app3.py
@@ -2,7 +2,7 @@
 #
 # print('1.Cappuciono ...4lei')
 # print('2.Espresso...3.5')
-#
+#products ={'1':4,'2':3.5}  # extragere cheie din dictionar
 # option = int(input('Ce optiune alegeti? [1,2]:'))
 # coin = int(input('Introduceti o bancnota [5,10]):'))
 # if option == 1:
@@ -21,6 +21,7 @@
 #varianta 2 automat cafea
 print('1.Cappuciono ...4lei')
 print('2.Espresso...3.5')
+products ={'1':4,'2':3.5}  # extragere cheie din dictionar
 
 option = int(input('Ce optiune alegeti? [1,2]:'))
 coin = int(input('Introduceti o bancnota [5,10]):'))
@@ -32,5 +33,8 @@ elif option == 2 and coin == 5:
         rest = 5 - 3.5
 elif option == 2 and  coin == 10:
         rest = 10 - 3.5
+else:
+        print('Invalid products')
+
 
 print(f'Veti primii restul de {rest} lei.')
