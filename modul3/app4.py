46e3e33a018ebfe32fd0c63d5a8aa23cb055c579
Leontin <leo_l3o@yahoo.com>
2024-02-07 20:24:58 +0200

    Primele proiecte

--- modul3/app4.py
+++ modul3/app4.py
@@ -1,12 +1,12 @@
-duplicated = [1,3,5,7,7,7,7,7,9,3]    # remove duplicated numbers from the list
+duplicated = [1, 3, 5, 7, 7, 7, 7, 7, 9, 3]  # remove duplicated numbers from the list
 
-new_list= []
-for number in duplicated:
+new_list = []
+for number in duplicated[::]:
     if number not in new_list:
         new_list.append(number)
     else:
         duplicated.remove(number)
         print(number, ' is duplicate')
-
+duplicated.sort()
 print(new_list)
 print(duplicated)
