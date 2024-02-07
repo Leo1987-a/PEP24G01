95d3c60a1c83f6fe3afd26734a1575edd8203543
Leontin <leo_l3o@yahoo.com>
2024-02-07 21:00:02 +0200

    Primele proiecte

---
+++ modul3/app5.py
@@ -0,0 +1,13 @@
+# problema prin for
+parola = 7677
+for incercare in range(9999):
+    parola_introdusa = int(input('Introdu parola: '))
+    if parola == parola_introdusa:
+        print('Acces interzis')
+        break
+    else:
+        print('Parola gresita')
+
+
+
+#problema cu while
