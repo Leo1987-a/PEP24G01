ab55305699739d85143ea5926a5527353479ee89
Leontin <leo_l3o@yahoo.com>
2024-02-21 19:15:00 +0200

    Exercitiu

---
+++ Modul5/part 5.1.py
@@ -0,0 +1,17 @@
+import math as m
+from math import  *
+# def m():
+#     pass
+
+# def log():
+#     pass
+print(m.log(100,10))
+print(log(100,10))
+
+from math import  *  # not recomanded
+print(ceil(100))
+
+print(f"Script name is {__name__}")
+# use import
+from modul4.app1 import check_passwd
+check_passwd()
--- Tema/Tema Modul 4. Problema1.py
+++ Tema/Tema Modul 4. Problema1.py
@@ -15,7 +15,7 @@
 
 #Dam valoarea minima lui x=10 iar cea maxima este egala cu 20.
 #Folosim functia while pentru a afisa valorile lui x pana la valoarea maxima 20.
-#Alocam valorii y valoare care a fost data in enunt y=3*x.iar dupa aceea vorm apela printu cu textul dorit.Printam rezultatul ecuatiei dar in cadrul functiei vom adauga fiecarul x care urmeaza +1
+#Alocam valorii y, valoarea care a fost data in enunt y=3*x.iar dupa aceea vom apela printu cu textul dorit.Printam rezultatul ecuatiei dar in cadrul functiei vom adauga fiecarul x care urmeaza +1
 #x = (3*x**2 -4*y+4)  care rezulta in (3*x**2-4*3*x+4)
 x=10
 while x <=20:
