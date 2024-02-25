180140451a44cc76dd6e7c78f0265483c467353d
Leontin <leo_l3o@yahoo.com>
2024-02-25 21:28:49 +0200

    Exercitiu

--- Tema/temaM5.4.py
+++ Tema/temaM5.4.py
@@ -2,20 +2,16 @@ from random import random
 
 from random import randint
 
-
-
 lista = []
 for x in range(6):
-  lista.append(int(input("Introduceti numerele castigatoare{}:".format(x+1))))
+    lista.append(int(input("Introduceti numerele castigatoare{}:".format(x + 1))))
 
 print(f'Numelele alese sunt: {lista}')
 print()
 
 win_num = []
 for x in range(6):
-  print(f'Numerele castigatoare sunt: {randint(1,49)}')
-
-
+    print(f'Numerele castigatoare sunt: {randint(1, 49)}')
 
 num_matches = 0
 for number in lista:
