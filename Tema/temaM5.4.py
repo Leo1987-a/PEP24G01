c350e533195e569a54d78a8cfefd920a9e491052
Leontin <leo_l3o@yahoo.com>
2024-02-26 11:52:35 +0200

    Exercitiu

--- Tema/temaM5.4.py
+++ Tema/temaM5.4.py
@@ -1,21 +1,41 @@
-from random import random
-
+#
 from random import randint
 
-lista = []
-for x in range(6):
-    lista.append(int(input("Introduceti numerele castigatoare{}:".format(x + 1))))
+numereLotto = []
+for i in range(0,6):
+    number =randint(1,50)
+    while number in numereLotto:
+        number = randint(1,50)
+    numereLotto.append(number)
+numereLotto.sort()
 
-print(f'Numelele alese sunt: {lista}')
-print()
+numereUser = []
+for i in range (0,6):
+    number = int(input('Introduceti 6 numere norocoase: '))
+    while (number in numereUser or number < 1 or number > 50):
+        print("Acest numar nu este valid")
+    numereUser.append(number)
 
-win_num = []
-for x in range(6):
-    print(f'Numerele castigatoare sunt: {randint(1, 49)}')
 
-num_matches = 0
-for number in lista:
-    if number in win_num:
-        num_matches += 1
-print("Ai castigat {} numere".format(num_matches))
-print("Numere asemanatoare {} numbers.".format(win_num))
+print("Numerele din extragerea de astazi sunt: ")
+print(numereLotto)
+print("Numerele dumneavoastra sunt: ")
+print(numereUser)
+
+counter = 0
+for number in numereUser:
+    if number in numereLotto:
+        counter +=1
+        print("Ati castigat 50Lei")
+    if number in numereLotto==2:
+        print("Ati castigat 50Lei")
+    if number in numereLotto ==3:
+        print("Ati castigat 50Lei")
+    if number in numereLotto == 4:
+        print("Ati castigat 500Lei")
+    if number in numereLotto == 5:
+        print("Ati castigat 500Lei")
+    if number in numereLotto == 6:
+        print("Ati castigat 1500Lei")
+print('Numerele identice sunt '+ str(counter))
+
